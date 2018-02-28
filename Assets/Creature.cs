7da91a21d403d14c5a1fc4b3b5c0676889054ef5
Bas van Seeters <bas.vanseeters@hotmail.com>
2018-02-28 12:20:43 +0100

    created the unity project

---
+++ Assets/Creature.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature {
+	public int b_agility;
+	public int agility;
+
+	public int b_strength;
+	public int strength;
+
+	public int intelligence;
+	public int b_intelligence;
+
+	public int age;
+	public int b_age;
+
+
+
+
+}
