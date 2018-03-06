6276e9f245575e57495a3a618d1aa0658a7729b6
Bas van Seeters <bas.vanseeters@hotmail.com>
2018-03-06 09:53:13 +0100

    All new updates
    sorry for pushing so late, but here is the project as far as i got with it.

--- Assets/Creature.cs
+++ Assets/Creature.cs
@@ -2,19 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Creature {
-	public int b_agility;
-	public int agility;
+public class Creature : MonoBehaviour{
+	public DNA Dna;
+	public enum ageMode {young, prime, decay};
+	public ageMode agemode;
 
-	public int b_strength;
-	public int strength;
+	//creature lifetime etc
 
-	public int intelligence;
-	public int b_intelligence;
+	
 
-	public int age;
-	public int b_age;
+	void OnTriggerEnter(Collision coll){
+		if (Random.Range (0, 1) == 1) {
+			if (Random.Range (0f, 0.1f * Dna.intelligence) == 1) {
+				Run (coll.gameObject);
+			} else {
+				Fight (coll.gameObject);
+			}
+		} else {
+			Mate (coll.gameObject);
+		}
+	}
 
+	void start(){
+		StartCoroutine (AgeDecay ());
+	}
+
+	void Update(){
+		if (Dna.age > 40) {
+			agemode = ageMode.prime;
+		}
+	}
+
+	public void Mate(GameObject collObj){
+		for (int i = 0; i < Random.Range (1f, 3f); i++) {
+			
+			Instantiate (gameObject, transform.position, transform.rotation);
+		}
+	}
+
+	public void Fight(GameObject collObj){
+		if (Dna.strength > collObj.GetComponent <Creature>().Dna.strength) {
+			Dna.AddStats (collObj.GetComponent <Creature>().Dna, 0.2f);
+		} else {
+			Destroy (gameObject, 0.3f);
+		}
+	}
+
+	public void Run(GameObject collObj){
+		//iets met de nav mesh agent ofzo
+	}
+
+	public IEnumerator AgeDecay(){
+		Dna.age = Dna.age + 1;
+		yield return new WaitForSeconds(1f);
+
+		switch (agemode) {
+		case ageMode.young:
+			Dna.age = Dna.age + 1;
+			yield return new WaitForSeconds(1f);
+			break;
+
+		case ageMode.prime:
+			yield return new WaitForSeconds (40f);
+			agemode = ageMode.decay;
+			break;
+
+		case ageMode.decay:
+			Dna.age = Dna.age - 1;
+			yield return new WaitForSeconds(1.8f);
+			break; 
+		}
+
+		StartCoroutine (AgeDecay ());
+	}
 
 
 
---
+++ Assets/DNA.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA {
+	public float agility;
+	public float strength;
+	public float intelligence;
+	public int age;
+
+	public DNA (float agility, float strength, float intelligence) {
+		this.agility = agility;
+		this.strength = strength;
+		this.intelligence = intelligence;
+		this.age = 0;
+	}
+
+	public DNA MergeStats(DNA otherDna){
+		float finalAG, finalST, finalIN;
+
+		if (agility < otherDna.agility) {
+			finalAG = Random.Range (agility, otherDna.agility +agility);
+		} else {
+			finalAG = Random.Range (otherDna.agility, agility + otherDna.agility);
+		}
+
+		if (strength < otherDna.strength) {
+			finalST = Random.Range (strength, otherDna.strength + strength);
+		} else {
+			finalST = Random.Range (otherDna.strength, strength + otherDna.strength);
+		}
+
+		if (intelligence < otherDna.intelligence) {
+			finalIN = Random.Range (intelligence, otherDna.intelligence + intelligence);
+		} else {
+			finalIN = Random.Range (otherDna.intelligence, intelligence + otherDna.intelligence);
+		}
+	
+		return new DNA (finalAG, finalST, finalIN);
+	}
+
+	public void AddStats(DNA otherDna, float statPercentage){
+		agility = agility + agility * statPercentage;
+		strength = strength + strength * statPercentage;
+		intelligence = intelligence + intelligence * statPercentage;
+
+
+	}
+
+}
